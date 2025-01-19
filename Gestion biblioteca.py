cf274b779494691fb68b5c8f081da050b24d3363
varetoo <alvaromi2005@gmail.com>
2025-01-19 15:33:05 +0100

    Unstable 0.1.2 Desarrollando botones de check

--- Gestion biblioteca.py
+++ Gestion biblioteca.py
@@ -32,6 +32,7 @@ class user:
     
     @nombre.setter
     def nombre(self, cadena):
+        print(cadena)
         self.__nombre = cadena
         
     @password.setter
@@ -41,6 +42,16 @@ class user:
     @prestamos_activos.setter
     def prestamos_activos(self, cadena):
         self.__prestamos_activos = cadena
+
+class checkbutton:
+    def __init__(self, estado = None):
+        self.__estado = estado
+    @property
+    def estado(self):
+        return self.__estado.get()
+    @estado.setter
+    def estado(self, cadena):
+        self.__estado = cadena
 
 
 #==================== BASES DE DATOS ====================
@@ -78,9 +89,9 @@ def change_screen(id = 0):  #Funcion que se encarga de cambiar de pantalla la in
         titulo.place(relx=0.02, rely=0.05, relwidth=0.9, relheight=0.1)
 
         # Usuario
-        label_usuario = tk.Label(ventana, text="Usuario:")
+        label_user = tk.Label(ventana, text="Usuario:")
         user_entry = tk.Entry(ventana)
-        label_usuario.place(relx=0.25, rely=0.23, relwidth=0.2, relheight=0.03)
+        label_user.place(relx=0.25, rely=0.23, relwidth=0.2, relheight=0.03)
         user_entry.place(relx=0.45, rely=0.23, relwidth=0.25, relheight=0.05)
 
         # Contraseña
@@ -90,7 +101,7 @@ def change_screen(id = 0):  #Funcion que se encarga de cambiar de pantalla la in
         password_entry.place(relx=0.45, rely=0.3, relwidth=0.25, relheight=0.05)
 
         # Botón ACEPTAR
-        boton_aceptar = tk.Button(ventana, text="Aceptar", command=lambda: change_screen(1))
+        boton_aceptar = tk.Button(ventana, text="Aceptar", command=compr_user)
         boton_aceptar.place(relx=0.45, rely=0.38, relwidth=0.25, relheight=0.05)
 
         # Mensaje y botón de REGÍSTRATE
@@ -99,7 +110,9 @@ def change_screen(id = 0):  #Funcion que se encarga de cambiar de pantalla la in
         mensaje_registro.place(relx=0.25, rely=0.6, relwidth=0.2, relheight=0.05)
         boton_registro.place(relx=0.45, rely=0.6, relwidth=0.25, relheight=0.05)
 
-        usuario = user(user_entry, password_entry, None, None, )
+        #Mandamos los valores a class: user
+        usuario.nombre = user_entry
+        usuario.password = password_entry
     
     elif id == -1:    #SING UP
         #Title
@@ -109,9 +122,9 @@ def change_screen(id = 0):  #Funcion que se encarga de cambiar de pantalla la in
         titulo.place(relx=0.02, rely=0.05, relwidth=0.9, relheight=0.1)
         
         # Usuario
-        label_usuario = tk.Label(ventana, text="Usuario:")
+        label_user = tk.Label(ventana, text="Usuario:")
         user_entry = tk.Entry(ventana)
-        label_usuario.place(relx=0.25, rely=0.23, relwidth=0.2, relheight=0.03)
+        label_user.place(relx=0.25, rely=0.23, relwidth=0.2, relheight=0.03)
         user_entry.place(relx=0.45, rely=0.23, relwidth=0.25, relheight=0.05)
         
         # Contraseña
@@ -122,8 +135,10 @@ def change_screen(id = 0):  #Funcion que se encarga de cambiar de pantalla la in
         
         #Premium ¿?
         label_premium = tk.Label(ventana, text="Premium por 4,99€/mes?")
-        check1_premium = tk.Checkbutton(ventana, text="Si")
-        check2_premium = tk.Checkbutton(ventana, text="No")
+        check1_premium = tk.Checkbutton(ventana, text="Si", command=lambda: checkbutton_no_iguales(1))
+        check1.estado = check1_premium
+        check2_premium = tk.Checkbutton(ventana, text="No", command=lambda: checkbutton_no_iguales(2))
+        check2.estado = check2_premium
         label_ventajas = tk.Label(ventana, text="Disfrutarás de los beneficios premium como tener más cantidad de libros prestados al\nmismo tiempo y poder pedir prestados todos los libros que tengas disponibles a la vez")
         
         label_premium.place( relx=0.20, rely=0.37, relwidth=0.25, relheight=0.03)
@@ -132,8 +147,12 @@ def change_screen(id = 0):  #Funcion que se encarga de cambiar de pantalla la in
         label_ventajas.place(relx=0.18, rely=0.44, relwidth=0.76, relheight=0.08)
         
         # Botón ACEPTAR
-        boton_aceptar = tk.Button(ventana, text="Aceptar", command=lambda: change_screen(1))
+        boton_aceptar = tk.Button(ventana, text="Aceptar", command=compr_user)
         boton_aceptar.place(relx=0.45, rely=0.60, relwidth=0.25, relheight=0.05)
+        
+        #Mandamos los valores a class: user
+        usuario.nombre = user_entry
+        usuario.password = password_entry
     
     
     elif id == 1:     #MAIN
@@ -152,20 +171,41 @@ def change_screen(id = 0):  #Funcion que se encarga de cambiar de pantalla la in
 
 
 #==================== CODIGO ====================
-#Funciones
-def compr_user():    #Comprueba si los datos introducidos concuerdan con la base de datos
-    user = password_entry.get()
-    pssw = 
+#Leemos los arcihvos y almacenamos los datos
+users_list = leer_arch("Datos/base_datos.txt")
+stock_list = leer_arch("Datos/stock_libros.txt")
 
+#Funciones
+def checkbutton_no_iguales(utlimo):
+    c1 = check1.estado
+    c2 = check2.estado
+
+
+
+
+def compr_user():    #Comprueba si los datos introducidos concuerdan con la base de datos
+    #Comprobamos si el usuario existe
+    name = usuario.nombre.get()
+    for elemento in users_list:
+        name_aux = elemento.nombre
+        if name_aux == name:
+            #El nombre está en la lista
+            messagebox.showinfo("Econtrado usuario", f"Se ha encontrado el nombre de usuario {name}")
+            encontrado = True
+            break
+        encontrado = False
+    if encontrado != True: messagebox.showerror("Error usuario", f"No se ha encontrado el nombre de usuario {name}")
         
     
     
 
 
-#Leemos los arcihvos y almacenamos los datos
-users_datos = leer_arch("Datos/base_datos.txt")
-stock_datos = leer_arch("Datos/stock_libros.txt")
 
+
+#Creamos el perfil del usuario vacio y los checkbuttons vacios
+usuario = user()
+check1 = checkbutton()
+check2 = checkbutton()
 
 #Iniciamos la primera pantalla
 change_screen(0)
--- pruebas.py
+++ pruebas.py
@@ -1,12 +1,27 @@
 class user:
     def __init__(self, nombre, contra, edad = None):
-        self.nombre = nombre
+        self.__nombre = nombre
         self.contra = contra
         self.edad = edad
+    
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self, cadena):
+        self.__nombre = cadena
 
 
 print("HOLA")
 usuario = user("Paco", "adowiha")
+
+
+print(f"nombre: {usuario.nombre}, contraseña: {usuario.contra}, edad:{usuario.edad}")
+
+
+usuario.nombre = "hola"
+
 
 
 print(f"nombre: {usuario.nombre}, contraseña: {usuario.contra}, edad:{usuario.edad}")
